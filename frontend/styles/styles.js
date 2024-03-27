e3f9993d0a8883e5db49f201db0df9809e923c92
RyouKinoshita <ryoukinoshita@gmail.com>
2024-03-28 05:19:02 +0800

    tweaks

--- frontend/styles/styles.js
+++ frontend/styles/styles.js
@@ -1,11 +1,11 @@
 import { StyleSheet, Platform, StatusBar } from "react-native";
 
 export const colors = {
-  color1: "#c70049",
+  color1: "#F4CE14",
   color1_light: "rgba(227,25,99,1)",
   color1_light2: "rgba(199,0,73,0.8)",
   color2: "white",
-  color3: "rgb(45,45,45)",
+  color3: "#45474B",
   color4: "transparent",
   color5: "#f2f2f2",
   color6: "#f7f7f7",
