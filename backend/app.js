c316589a57c5f87dfa6adb98941d3b8e611051c8
RyouKinoshita <ryoukinoshita@gmail.com>
2024-03-29 11:22:24 +0800

    tweaks

--- backend/app.js
+++ backend/app.js
@@ -27,11 +27,11 @@ app.get("/", (req, res, next) => {
 
 // Importing Routers here
 import user from "./routes/user.js";
-import product from "./routes/product.js";
+import service from "./routes/service.js";
 import order from "./routes/order.js";
 
 app.use("/api/v1/user", user);
-app.use("/api/v1/product", product);
+app.use("/api/v1/service", service);
 app.use("/api/v1/order", order);
 
 // Using Error Middleware
