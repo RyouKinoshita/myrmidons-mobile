adb52f300b6ef9f87c440ed28a54ad2c42beab66
RyouKinoshita <ryoukinoshita@gmail.com>
2024-03-29 11:46:39 +0800

    redux initial commit

--- frontend/App.js
+++ frontend/App.js
@@ -1,8 +1,11 @@
-import { View, Text } from "react-native";
 import Main from "./Main";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
-const App = () => {
-  return <Main />;
-};
-
-export default App;
+export default function App() {
+  return (
+    <Provider store={store}>
+      <Main />
+    </Provider>
+  );
+}
