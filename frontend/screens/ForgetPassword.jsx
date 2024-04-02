4569e60ed78ec31a219ec46c84cc5d3c454bf61b
RyouKinoshita <ryoukinoshita@gmail.com>
2024-04-02 17:02:00 +0800

    forgot password

--- frontend/screens/ForgetPassword.jsx
+++ frontend/screens/ForgetPassword.jsx
@@ -1,97 +1,115 @@
-import React, { useState } from 'react';
-import { ScrollView, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
-import { TextInput, Button } from 'react-native-paper';
-import { colors } from '../styles/styles';
-import Footer from '../components/Footer';
+import React, { useState } from "react";
+import {
+  View,
+  ScrollView,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { TextInput, Button } from "react-native-paper";
+import { colors, formHeading } from "../styles/styles";
+import Footer from "../components/Footer";
+import { useDispatch } from "react-redux";
+import { forgetPassword } from "../redux/actions/otherAction";
+import { useMessageAndErrorOther } from "../utils/hooks";
 
 const ForgetPassword = ({ navigation }) => {
-    const [email, setEmail] = useState('');
-    // const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const dispatch = useDispatch();
 
-    const loading = false
-    const submitHandler = () => {
-        alert("OTP send to you Email");
-        navigation.navigate("verify")
-    };
+  const loading = useMessageAndErrorOther(dispatch, navigation, "verify");
 
-    return (
-        <>
-        <ScrollView contentContainerStyle={styles.container}>
-            <Image
-                source={require('../assets/logo.png')}
-                style={styles.logo}
-                resizeMode="contain"
-            />
-            <TextInput
-                label="Email"
-                value={email}
-                onChangeText={setEmail}
-                keyboardType="email-address"
-                style={styles.input}
-               
-            />
+  const submitHandler = () => {
+    dispatch(forgetPassword(email));
+  };
+  return (
+    <>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Image
+          source={require("../assets/logo.png")}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+        <View style={{ marginBottom: 20 }}>
+          <Text style={styles.heading}>Forgot Password</Text>
+        </View>
+        <TextInput
+          label="Email"
+          value={email}
+          onChangeText={setEmail}
+          keyboardType="email-address"
+          style={styles.input}
+        />
 
-            <Button
-                mode="contained"
-                onPress={submitHandler}
-                style={styles.loginButton}
-                labelStyle={{ color: colors.color2 }}
-            >
-                Send OTP
-            </Button>
-            <TouchableOpacity style={styles.signUp} onPress={() => navigation.navigate('signup')}>
-                <Button
-                    mode="text"
-                    onPress={() => navigation.navigate('signup')}
-                    labelStyle={{ color: colors.color3 }}
-                >
-                    Don't have an account? Sign Up
-                </Button>
-            </TouchableOpacity>
-
-
-        </ScrollView>
-        <Footer/>
-        </>
-    );
+        <Button
+          loading={loading}
+          disabled={email === ""}
+          mode="contained"
+          onPress={submitHandler}
+          style={styles.loginButton}
+          labelStyle={{ color: colors.color2 }}
+        >
+          Send OTP
+        </Button>
+        <TouchableOpacity
+          style={styles.signUp}
+          onPress={() => navigation.navigate("signup")}
+        >
+          <Button
+            mode="text"
+            onPress={() => navigation.navigate("signup")}
+            labelStyle={{ color: colors.color3 }}
+          >
+            Don't have an account? Sign Up
+          </Button>
+        </TouchableOpacity>
+      </ScrollView>
+      <Footer activeRoute="profile" />
+    </>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flexGrow: 1,
-        padding: 20,
-        backgroundColor: colors.color1,
-        justifyContent: 'center',
-    },
-    input: {
-        marginBottom: 10,
-        backgroundColor: colors.color2,
-    },
-    forgot: {
-        textAlign: 'right',
-        color: colors.color3,
-        textDecorationLine: 'underline',
-        marginBottom: 10,
-    },
-    loginButton: {
-        marginTop: 20,
-        backgroundColor: colors.color3,
-    },
-    signUp: {
-        marginTop: 20,
-        alignItems: 'center',
-    },
-    signUpText: {
-        color: colors.color3,
-        textDecorationLine: 'underline',
-    },
-    logo: {
-        height: 150,
-        width: '100%',
-        marginBottom: 20,
-    },
+  container: {
+    flexGrow: 1,
+    padding: 20,
+    backgroundColor: colors.color1,
+    justifyContent: "center",
+  },
+  input: {
+    marginBottom: 10,
+    backgroundColor: colors.color2,
+  },
+  forgot: {
+    textAlign: "right",
+    color: colors.color3,
+    textDecorationLine: "underline",
+    marginBottom: 10,
+  },
+  loginButton: {
+    marginTop: 20,
+    backgroundColor: colors.color3,
+  },
+  signUp: {
+    marginTop: 20,
+    alignItems: "center",
+  },
+  signUpText: {
+    color: colors.color3,
+    textDecorationLine: "underline",
+  },
+  logo: {
+    height: 150,
+    width: "100%",
+    marginBottom: 20,
+  },
+  heading: {
+    fontWeight: "bold",
+    fontSize: 24,
+    color: colors.color3,
+    marginTop: 20,
+  },
 });
 
-
-
-export default ForgetPassword
+export default ForgetPassword;
--- frontend/screens/Verify.jsx
+++ frontend/screens/Verify.jsx
@@ -1,104 +1,110 @@
-import React, { useState } from 'react';
-import { ScrollView, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
-import { TextInput, Button } from 'react-native-paper';
-import { colors } from '../styles/styles';
-import Footer from '../components/Footer';
+import React, { useState } from "react";
+import {
+  ScrollView,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { TextInput, Button } from "react-native-paper";
+import { colors } from "../styles/styles";
+import Footer from "../components/Footer";
+import { useMessageAndErrorOther } from "../utils/hooks";
+import { useDispatch } from "react-redux";
+import { resetPassword } from "../redux/actions/otherAction";
 
 const Verify = ({ navigation }) => {
-    const [otp, setOtp] = useState('');
-    const [password, setPassword] = useState('');
+  const [otp, setOtp] = useState("");
+  const [password, setPassword] = useState("");
 
-    const loading = false
-    const submitHandler = () => {
-        alert("Reset Success");
-        navigation.navigate("login")
-    };
+  const dispatch = useDispatch();
+  const loading = useMessageAndErrorOther(dispatch, navigation, "login");
 
-    return (
-        <>
-        <ScrollView contentContainerStyle={styles.container}>
-            <Image
-                source={require('../assets/logo.png')}
-                style={styles.logo}
-                resizeMode="contain"
-            />
-            <TextInput
-                label="OTP"
-                value={otp}
-                onChangeText={setOtp}
-                keyboardType="number-pad"
-                style={styles.input}
-            
-            />
-            <TextInput
-                label="New Password"
-                value={password}
-                onChangeText={setPassword}
-                style={styles.input}
-               
-            />
-            <Button
-                mode="contained"
-                onPress={submitHandler}
-                disabled={otp === "" || password === ""}
-                style={styles.loginButton}
-                labelStyle={{ color: colors.color2 }}
-            >
-                Reset
-            </Button> 
-        
-            <Button
-                mode="text"
-                onPress= {() => navigation.navigate("forgetpassword")}
-                labelStyle={{ color: colors.color3 }}
-                style={{ alignSelf: 'center', marginTop: 20 }}
-            >
-                Resend OTP
-            </Button>
+  const submitHandler = () => {
+    dispatch(resetPassword(otp, password));
+  };
+  return (
+    <>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Image
+          source={require("../assets/logo.png")}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+        <TextInput
+          label="OTP"
+          value={otp}
+          onChangeText={setOtp}
+          keyboardType="number-pad"
+          style={styles.input}
+          secureTextEntry={true}
+        />
+        <TextInput
+          label="New Password"
+          value={password}
+          onChangeText={setPassword}
+          style={styles.input}
+          secureTextEntry={true}
+        />
+        <Button
+          loading={loading}
+          mode="contained"
+          onPress={submitHandler}
+          disabled={otp === "" || password === ""}
+          style={styles.loginButton}
+          labelStyle={{ color: colors.color2 }}
+        >
+          Reset
+        </Button>
 
-        </ScrollView>
-        <Footer/>
-        </>
-    );
+        <Button
+          mode="text"
+          onPress={() => navigation.navigate("forgetpassword")}
+          labelStyle={{ color: colors.color3 }}
+          style={{ alignSelf: "center", marginTop: 20 }}
+        >
+          Resend OTP
+        </Button>
+      </ScrollView>
+      <Footer />
+    </>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flexGrow: 1,
-        padding: 20,
-        backgroundColor: colors.color1,
-        justifyContent: 'center',
-    },
-    input: {
-        marginBottom: 10,
-        backgroundColor: colors.color2,
-    },
-    forgot: {
-        textAlign: 'right',
-        color: colors.color3,
-        textDecorationLine: 'underline',
-        marginBottom: 10,
-    },
-    loginButton: {
-        marginTop: 20,
-        backgroundColor: colors.color3,
-    },
-    signUp: {
-        marginTop: 20,
-        alignItems: 'center',
-    },
-    signUpText: {
-        color: colors.color3,
-        textDecorationLine: 'underline',
-    },
-    logo: {
-        height: 150,
-        width: '100%',
-        marginBottom: 20,
-    },
+  container: {
+    flexGrow: 1,
+    padding: 20,
+    backgroundColor: colors.color1,
+    justifyContent: "center",
+  },
+  input: {
+    marginBottom: 10,
+    backgroundColor: colors.color2,
+  },
+  forgot: {
+    textAlign: "right",
+    color: colors.color3,
+    textDecorationLine: "underline",
+    marginBottom: 10,
+  },
+  loginButton: {
+    marginTop: 20,
+    backgroundColor: colors.color3,
+  },
+  signUp: {
+    marginTop: 20,
+    alignItems: "center",
+  },
+  signUpText: {
+    color: colors.color3,
+    textDecorationLine: "underline",
+  },
+  logo: {
+    height: 150,
+    width: "100%",
+    marginBottom: 20,
+  },
 });
 
-
-
-
-export default Verify
+export default Verify;
